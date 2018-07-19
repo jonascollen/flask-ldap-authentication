81c0222c49a8d6016846e4ab89fcec4e313d13b8
Jonas Collén <40911794+jonascollen@users.noreply.github.com>
2018-07-19 15:17:09 +0200

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,19 @@
+from main import app
+from flask import render_template, request
+import ldap
+
+def authenticate(username, password):
+    LDAP_SERVER = 'ldap://xx'
+    LDAP_USERNAME = 'DOMAIN\\%s' % username
+    LDAP_PASSWORD = password
+
+    # Tries to validate user with LDAP-bind, returns OK if valid
+    try:
+        ldap_client = ldap.initialize(LDAP_SERVER)
+        ldap_client.set_option(ldap.OPT_REFERRALS,0)
+        ldap_client.simple_bind_s(LDAP_USERNAME, LDAP_PASSWORD)
+    except:
+        ldap_client.unbind()
+        return 'PASSWORD_ERROR'
+    ldap_client.unbind()
+    return 'OK
